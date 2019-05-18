1d33b543deb3ced6da8ef5b9193ca96b3cb9c918
Mohammad Ali Riazati <>
2019-05-18 11:57:05 +0430

    Initial commit
    Initial commit

--- app/src/main/java/com/ticka/application/models/HomesModel.java
+++ app/src/main/java/com/ticka/application/models/HomesModel.java
@@ -6,8 +6,6 @@ public class HomesModel implements Serializable {
 
     private static HomesModel homesModel = null;
 
-    private String d = "";
-
     public static HomesModel getInstance(){
         if(homesModel == null){
             homesModel = new HomesModel();
@@ -15,11 +13,29 @@ public class HomesModel implements Serializable {
         return homesModel;
     }
 
-    private HomesModel(){
-
-    }
-
-
+    private String homeTitle;
+    private Integer homeStateId;
+    private Integer homeCityId;
+    private String homeAddress;
+    private String homeDescription;
+    private String buildingType;
+    private String locationType;
+    private String buildingTip;
+    private String roomNubmer;
+    private Integer landArea;
+    private Integer buildingArea;
+    private String photoLocation1;
+    private String photoLocation2;
+    private String photoLocation3;
+    private Integer standardCapacity;
+    private Integer maximumCapacity;
+    private Integer pricePerPersonAdded;
+    private Integer singleBed;
+    private Integer doubleBed;
+    private Integer matsAdded;
+    private String capacityDescription;
+    private String possibilitiesDescriptiopn;
+    private String ruleDescriptiopn;
 
 
 }
