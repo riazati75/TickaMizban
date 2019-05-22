8138adad2ba577efe585f7c9686375920be690e4
Mohammad Ali Riazati <>
2019-05-22 19:46:31 +0430

    Initial commit
    Initial commit

--- app/src/main/java/com/ticka/application/fragments/UserRulesFragment.java
+++ app/src/main/java/com/ticka/application/fragments/UserRulesFragment.java
@@ -9,14 +9,21 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.stepstone.stepper.BlockingStep;
 import com.stepstone.stepper.StepperLayout;
 import com.stepstone.stepper.VerificationError;
 import com.ticka.application.R;
 import com.ticka.application.adapters.CheckboxAdapter;
+import com.ticka.application.core.Logger;
+import com.ticka.application.models.HomeDataModel;
 
 public class UserRulesFragment extends Fragment implements BlockingStep {
+
+    private HomeDataModel homesModel = HomeDataModel.getInstance();
+    private CheckboxAdapter adapter;
+    private EditText inputDescription;
 
     public UserRulesFragment() {
         // Required empty public constructor
@@ -27,11 +34,13 @@ public class UserRulesFragment extends Fragment implements BlockingStep {
 
         View root = inflater.inflate(R.layout.fragment_user_rules, container, false);
 
+        inputDescription = root.findViewById(R.id.inputDescription);
+
         RecyclerView recyclerView = root.findViewById(R.id.recycler);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
         recyclerView.setHasFixedSize(true);
 
-        CheckboxAdapter adapter = new CheckboxAdapter(getContext(), CheckboxAdapter.RULES_TYPE);
+        adapter = new CheckboxAdapter(getContext(), CheckboxAdapter.RULES_TYPE);
         recyclerView.setAdapter(adapter);
 
         return root;
@@ -55,7 +64,15 @@ public class UserRulesFragment extends Fragment implements BlockingStep {
     @Nullable
     @Override
     public VerificationError verifyStep() {
-        return null;
+        if(adapter.getSelectedList().size() <= 3){
+            return new VerificationError("حداقل 4 مورد را انتخاب کنید ");
+        }
+        else {
+            homesModel.setFacilitiesArray(adapter.getSelectedList());
+            homesModel.setFacilitiesDescription(inputDescription.getText().toString());
+            Logger.Log(homesModel.parsData());
+            return null;
+        }
     }
 
     @Override
