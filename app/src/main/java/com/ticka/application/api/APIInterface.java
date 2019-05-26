6326b9a011fdc17f472fdc378495b5c6482d5d25
Mohammad Ali Riazati <>
2019-05-26 17:22:48 +0430

    Initial commit
    Initial commit

--- app/src/main/java/com/ticka/application/api/APIInterface.java
+++ app/src/main/java/com/ticka/application/api/APIInterface.java
@@ -5,18 +5,14 @@ import com.ticka.application.models.facility.FacilityModel;
 import com.ticka.application.models.home.HomeModel;
 import com.ticka.application.models.rules.RuleData;
 
-import java.io.File;
 import java.util.List;
 
 import okhttp3.RequestBody;
-import okhttp3.ResponseBody;
 import retrofit2.Call;
-import retrofit2.Response;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.Headers;
 import retrofit2.http.POST;
-import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface APIInterface {
